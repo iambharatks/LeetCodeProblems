225cf5bba95b455564196013447fb7141360e940
iambharatks <iambharatks@gmail.com>
2021-01-04 15:44:58 +0530

    merge two sorted lists

---
+++ mergeTwoSortedLists.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        ListNode *l = NULL, *prev;
+        while (l1 && l2)
+        {
+            if (l1->val <= l2->val)
+            {
+                if (l == NULL)
+                    l = l1;
+                else
+                    prev->next = l1;
+                prev = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                if (l == NULL)
+                    l = l2;
+                else
+                    prev->next = l2;
+                prev = l2;
+                l2 = l2->next;
+            }
+        }
+
+        if (l1)
+        {
+            if (l == NULL)
+                return l1;
+            else
+                prev->next = l1;
+            return l;
+        }
+        if (l2)
+        {
+            if (l == NULL)
+                return l2;
+            else
+                prev->next = l2;
+            return l;
+        }
+        return l;
+    }
+};
