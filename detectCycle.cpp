e464882892e4d7fa49b58d9daf7eab373dc68fc9
iambharatks <iambharatks@gmail.com>
2021-02-03 23:49:37 +0530

    Linked List Cycle

---
+++ detectCycle.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//   Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head;
+        if (head == NULL)
+            return false;
+        while (slow->next && fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
