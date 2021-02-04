7e5ca47c467ee6ad300ba6bc9e369ad0074b6ec6
iambharatks <iambharatks@gmail.com>
2021-02-04 18:11:45 +0530

    Word Ladder

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -44,7 +44,67 @@ public:
         unordered_set<string> words;
         for (string word : wordList)
             words.insert(word);
-        return shortestChain(beginWord, endWord, words);
+        int i = shortestChain(beginWord, endWord, words);
+        for (string s : words)
+            cout << s << " ";
+        return i;
+    }
+};
+//TLE Solution
+class SolutionNaive
+{
+    bool isConnected(string u, string v)
+    {
+        int cnt = 0;
+        for (int i = 0; i < u.size(); i++)
+        {
+            if (u[i] != v[i])
+                cnt++;
+        }
+        return cnt == 1;
+    }
+    bool search(vector<string> &wordList, string word)
+    {
+        for (string s : wordList)
+            if (s == word)
+                return true;
+        return false;
+    }
+
+public:
+    int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+    {
+        if (!search(wordList, endWord))
+            return 0;
+        queue<string> q;
+        unordered_map<string, bool> vis;
+        q.push(beginWord);
+        vis[beginWord] = true;
+        int nodes_left_in_layer = 1, nodes_in_next_layer = 0;
+        int operations = 0;
+        while (!q.empty())
+        {
+            string v = q.front();
+            q.pop();
+            for (string s : wordList)
+            {
+                if (!vis[s] && isConnected(v, s))
+                {
+                    if (s == endWord)
+                        return operations + 2;
+                    vis[s] = true;
+                    q.push(s);
+                    nodes_in_next_layer++;
+                }
+            }
+            nodes_left_in_layer--;
+            if (nodes_left_in_layer == 0)
+            {
+                swap(nodes_left_in_layer, nodes_in_next_layer);
+                operations++;
+            }
+        }
+        return -1;
     }
 };
 
---
+++ longestHarmoniousSubsequence.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution
+{
+public:
+    int findLHS(vector<int> &nums)
+    {
+        
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+}
