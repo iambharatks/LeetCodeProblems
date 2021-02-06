6fce965271fea4ac7c21a4e06412cc59828efe9f
iambharatks <iambharatks@gmail.com>
2021-02-06 21:22:47 +0530

    Find the number of islands

--- numberOfIslands.cpp
+++ numberOfIslands.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-// Number of Islands
-
-class Solution
+// Find the number of islands
+//BFS
+class Solution1
 {
 public:
     void bfs(int r, int c, vector<vector<char>> &grid, vector<vector<bool>> &visited)
     {
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, 1, -1};
+        int dr[] = {-1, 1, 0, 0, -1, 1, 1, -1};
+        int dc[] = {0, 0, 1, -1, -1, 1, -1, 1};
         queue<int> qr, qc;
         int n = grid.size();
         int m = grid[0].size();
@@ -22,7 +22,7 @@ public:
             c = qc.front();
             qr.pop();
             qc.pop();
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 8; i++)
             {
                 int rr = r + dr[i];
                 int cc = c + dc[i];
@@ -58,3 +58,47 @@ public:
         return numOfIslands;
     }
 };
+//better
+//dfs
+class Solution2
+{
+public:
+    void dfs(vector<vector<char>> &grid, int i, int j, int n, int m)
+    {
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j, n, m);
+        dfs(grid, i + 1, j, n, m);
+        dfs(grid, i, j - 1, n, m);
+        dfs(grid, i, j + 1, n, m);
+    }
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    ans++;
+                    dfs(grid, i, j, n, m);
+                }
+            }
+        }
+        return ans;
+    }
+};
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> grid(n, vector<char>(m));
+    for (auto &v : grid)
+        for (char &c : v)
+            cin >> c;
+    cout << Solution2().numIslands(grid);
+}
