e917f5917d3fdcc4e0bdc5a11652900c9857dc3e
iambharatks <iambharatks@gmail.com>
2021-02-17 20:28:34 +0530

    Letter Case Permutation

---
+++ letterCasePermutation.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void traverse(string &s, int i, vector<string> &ans, string res = "")
+    {
+        if (i == s.size())
+        {
+            ans.push_back(res);
+            return;
+        }
+        if (s[i] >= '0' && s[i] <= '9')
+            return traverse(s, i + 1, ans, res + s[i]);
+        else
+        {
+            traverse(s, i + 1, ans, res + s[i]);
+            if (s[i] >= 'A' && s[i] <= 'Z')
+                traverse(s, i + 1, ans, res + char(s[i] + 32));
+            else
+                traverse(s, i + 1, ans, res + char(s[i] - 32));
+        }
+    }
+    vector<string> letterCasePermutation(string S)
+    {
+        vector<string> ans;
+        traverse(S, 0, ans);
+        return ans;
+    }
+};
+
+int main()
+{
+    string s;
+    cin >> s;
+    vector<string> perm = Solution().letterCasePermutation(s);
+    for (string word : perm)
+        cout << word << " ";
+}
