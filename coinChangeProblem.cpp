55e66e09118cdc6bb9f3be144a9f6fac3945e66f
iambharatks <iambharatks@gmail.com>
2021-03-31 10:47:24 +0530

    Coin Change Problem

--- coinChangeProblem.cpp
+++ coinChangeProblem.cpp
@@ -25,7 +25,40 @@ public:
         return dp[amount];
     }
 };
-
+class BFS
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        unordered_set<int> qset;
+        queue<pair<int, int>> q;
+        qset.insert(amount);
+        //{coins,amount}
+        q.push({0, amount});
+        while (!q.empty())
+        {
+            int numCoins = q.front().first;
+            int remaining = q.front().second;
+            q.pop();
+            if (remaining == 0)
+                return numCoins;
+            if (remaining < 0)
+                continue;
+            for (int coin : coins)
+            {
+                int x = remaining - coin;
+                if (x < 0)
+                    continue;
+                if (qset.find(x) == qset.end())
+                {
+                    q.push({numCoins + 1, x});
+                    qset.insert(x);
+                }
+            }
+        }
+        return -1;
+    }
+};
 int main()
 {
     int n, amount;
@@ -33,5 +66,5 @@ int main()
     vector<int> coins(n);
     for (int &coin : coins)
         cin >> coin;
-    cout << Solution().coinChange(coins, amount);
+    cout << BFS().coinChange(coins, amount);
 }
