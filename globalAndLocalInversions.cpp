4dd5b044360789bd0499acf8ed22947274dc31cb
iambharatks <iambharatks@gmail.com>
2021-04-06 12:20:39 +0530

    Global and Local Inversions

--- globalAndLocalInversions.cpp
+++ globalAndLocalInversions.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
+//   Global and Local Inversions
+
 class Solution
 {
 public:
-    bool overlapping(){
-        
-    }
     bool isIdealPermutation(vector<int> &A)
     {
+        for (int i = 0; i < A.size(); i++)
+        {
+            if (abs(i - A[i]) > 1)
+                return false;
+        }
+        return true;
     }
 };
-
-int main()
-{
-}
