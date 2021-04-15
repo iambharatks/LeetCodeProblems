9ac30bb0ed025f3819cb25ff4f5f4596b42d2350
iambharatks <iambharatks@gmail.com>
2021-04-15 20:17:41 +0530

    Fibonacci Number

--- fibonacciNumbers.cpp
+++ fibonacciNumbers.cpp
@@ -10,7 +10,9 @@ public:
     int fib(int n)
     {
         int a = 0, b = 1, c = 1;
-        for (int i = 1; i <= n; i++)
+        if (n == 0)
+            return 0;
+        for (int i = 2; i <= n; i++)
         {
             c = a + b;
             a = b;
