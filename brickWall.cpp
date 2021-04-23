41e8f40d1af3cc3f047f461e6066a895d69d71eb
iambharatks <iambharatks@gmail.com>
2021-04-23 09:16:51 +0530

    Brick Wall

---
+++ brickWall.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//   Brick Wall
+
+class Solution
+{
+public:
+    int leastBricks(vector<vector<int>> &wall)
+    {
+        int width = 0, ans = 0;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < wall.size(); i++)
+        {
+            width = 0;
+            for (int j = 0; j < wall[i].size() - 1; j++)
+            {
+                width += wall[i][j];
+                mp[width] += 1;
+            }
+        }
+        for (auto it : mp)
+        {
+            cout << it.first << " " << it.second << "\n";
+            if (it.second > ans)
+                ans = it.second;
+        }
+        return wall.size() - ans;
+    }
+};
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    vector<vector<int>> wall(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> m;
+        wall[i].resize(m);
+        for (int j = 0; j < m; j++)
+            cin >> wall[i][j];
+    }
+    cout << Solution().leastBricks(wall);
+}
