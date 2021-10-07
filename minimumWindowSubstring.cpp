c4fc1d91b9268e6aa9fb52f254996657c17c146c
iambharatks <iambharatks@gmail.com>
2021-10-07 16:32:40 +0530

    76. Minimum Window Substring

--- minimumWindowSubstring.cpp
+++ minimumWindowSubstring.cpp
@@ -44,3 +44,68 @@ public:
         return (subStart == -1) ? "" : s.substr(subStart, minSize);
     }
 };
+
+class OptimisedSoln
+{
+public:
+    string minWindow(string s, string t)
+    {
+        if (s.size() < t.size())
+            return "";
+        // if (s == t)
+        //     return s;
+        vector<pair<int, char>> filteredS;
+        vector<int> mp(256);
+        for (int i = 0; i < t.size(); i++)
+        {
+            mp[t[i]]--;
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (mp[s[i]] < 0)
+                filteredS.push_back({i, s[i]});
+        }
+        if (filteredS.size() == 0)
+            return "";
+        int l = -1, ans = s.size()+2, start = -1;
+        for (int i = 0; i < filteredS.size(); i++)
+        {
+            mp[filteredS[i].second]++;
+            if (l == -1)
+                l = filteredS[i].first;
+            cout << filteredS[i].first << " " << filteredS[i].second << '\n';
+            bool fullfilled = true;
+            for (int j = 0; j < 256; j++)
+            {
+                if (mp[j] < 0)
+                {
+                    fullfilled = false;
+                    break;
+                }
+            }
+            if (fullfilled)
+            {
+                while (mp[filteredS[l].second] > 0)
+                {
+                    // cout << (filteredS[l].second) << ' ';
+                    mp[filteredS[l].second]--;
+                    l++;
+                }
+                if (ans > filteredS[i].first - filteredS[l].first + 1)
+                {
+                    ans = filteredS[i].first - filteredS[l].first + 1;
+                    start = filteredS[l].first;
+                }
+            }
+        }
+        // cout << start << " " << ans;
+        return (ans == -1) ? "" : s.substr(start, ans);
+    }
+};
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    cout << OptimisedSoln().minWindow(s, t);
+}
