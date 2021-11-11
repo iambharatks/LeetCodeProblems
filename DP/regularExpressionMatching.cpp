41a21f5a01cc933659156f407ea029719c0660c6
iambharatks <iambharatks@gmail.com>
2021-11-11 22:36:05 +0530

    10. Regular Expression Matching

--- DP/regularExpressionMatching.cpp
+++ DP/regularExpressionMatching.cpp
@@ -45,4 +45,43 @@ public:
         }
         return rec(s, p, s.size(), p.size());
     }
+    bool isMatchbu(string s, string p)
+    {
+        dp = new int *[s.size() + 1];
+        for (int i = 0; i <= s.size(); i++)
+        {
+            dp[i] = new int[p.size() + 1];
+            for (int j = 0; j < p.size() + 1; j++)
+            {
+                dp[i][j] = 0;
+            }
+        }
+        dp[0][0] = 1;
+        for (int i = 0; i <= s.size(); i++)
+        {
+            for (int j = 0; j <= p.size(); j++)
+            {
+                if (i == 0)
+                {
+                    if (j != 0 && p[j - 1] == '*')
+                        dp[i][j] = dp[i][j - 2];
+                    else
+                        dp[i][j] = 0;
+                }
+                else if(j == 0 && i!= 0)
+                    dp[i][j] = 0;
+                else if (s[i - 1] == p[j - 1] || p[j - 1] == '.')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if (p[j - 1] == '*')
+                {
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.')
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    else
+                        dp[i][j] = dp[i][j - 2];
+                }
+                else
+                    dp[i][j] = false;
+            }
+        }
+    }
 };
