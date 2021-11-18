31130d4e84a33ca8ceafa58e9d9967769d31f5f7
iambharatks <iambharatks@gmail.com>
2021-11-18 18:36:15 +0530

    139. Word Break

--- DP/wordBreak.cpp
+++ DP/wordBreak.cpp
@@ -31,3 +31,31 @@ public:
         return wordBreak(s, 0);
     }
 };
+
+class SolutionOptimised
+{
+public:
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        unordered_set<string> dict;
+        for (string s : wordDict)
+            dict.insert(s);
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (dp[j])
+                {
+                    if (dict.find(s.substr(j, i - j)) != dict.end())
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp.back();
+    }
+};
