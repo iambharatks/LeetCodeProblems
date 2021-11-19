ea1f1c69b5cbc0bd8c76f6a6180d16fa431d3512
iambharatks <iambharatks@gmail.com>
2021-11-19 09:03:51 +0530

    279. Perfect Squares

--- DP/perfectSquares.cpp
+++ DP/perfectSquares.cpp
@@ -32,3 +32,22 @@ public:
         return dp[n][k];
     }
 };
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        if (n == 1)
+            return 1;
+        vector<int> dp(n + 1, INT_MAX - 1);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j * j <= i; j++)
+            {
+                dp[i] = min(dp[i - j * j] + 1, dp[i]);
+            }
+        }
+        return dp[n];
+    }
+};
