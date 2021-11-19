8ccda9ec9ce5e63a64aa7af0e9e0a6703f210ea8
iambharatks <iambharatks@gmail.com>
2021-11-19 08:54:06 +0530

    279. Perfect Squares

---
+++ DP/perfectSquares.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        if (n <= 1)
+            return n;
+        int k = sqrt(n) + 1;
+        int dp[n + 1][k + 1];
+        for (int i = 0; i <= k; i++)
+        {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i][0] = INT_MAX - 1;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= k; j++)
+            {
+                if (i >= j * j)
+                    dp[i][j] = min(dp[i - j * j][j] + 1, dp[i][j - 1]);
+                else
+                    dp[i][j] = dp[i][j - 1];
+            }
+        }
+        return dp[n][k];
+    }
+};
