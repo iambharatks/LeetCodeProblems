5fc93763312f39a55890465af5f5d7ef5fd5f9b6
iambharatks <iambharatks@gmail.com>
2021-11-19 05:55:32 +0530

    188. Best Time to Buy and Sell Stock IV

--- DP/buySellStockIV.cpp
+++ DP/buySellStockIV.cpp
@@ -37,3 +37,21 @@ public:
         return maxProfit(k, prices, 0, true);
     }
 };
+
+class Solution
+{
+public:
+    int maxProfit(int k, vector<int> &prices)
+    {
+        vector<int> buy(k + 1, INT_MIN), sell(k + 1, 0);
+        for (int i = 0; i < prices.size(); i++)
+        {
+            for (int j = 1; j <= k; j++)
+            {
+                buy[j] = max(buy[j], sell[j - 1] - prices[i]);
+                sell[j] = max(sell[j], buy[j] + prices[i]);
+            }
+        }
+        return sell[k];
+    }
+};
