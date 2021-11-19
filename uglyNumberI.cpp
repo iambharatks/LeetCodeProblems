f1935d1bf2dc845e45ca4cbfc1e451f594a5ddfb
iambharatks <iambharatks@gmail.com>
2021-11-19 08:11:44 +0530

    263. Ugly Number

---
+++ uglyNumberI.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n == 0)
+            return false;
+        while ((n & 1) == 0)
+            n >>= 1;
+        while (n % 3 == 0)
+            n /= 3;
+        while (n % 5 == 0)
+            n /= 5;
+        return n == 1;
+    }
+};
