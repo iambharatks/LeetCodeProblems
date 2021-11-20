fc4e491734f84ae7aa709f4224f36dbcef1186cd
iambharatks <iambharatks@gmail.com>
2021-11-20 10:47:33 +0530

    309. Best Time to Buy and Sell Stock with Cooldown

--- DP/buySellStockWithCooldown.cpp
+++ DP/buySellStockWithCooldown.cpp
@@ -29,3 +29,26 @@ public:
         return maxProfit(prices, 0, true);
     }
 };
+// Great Approach
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        int s0[n], s1[n], s2[n];
+        memset(s0, 0, sizeof(s0));
+        memset(s1, 0, sizeof(s1));
+        memset(s2, 0, sizeof(s2));
+        s0[0] = 0;
+        s1[0] = -prices[0];
+        s2[0] = INT_MIN;
+        for (int i = 1; i < n; i++)
+        {
+            s0[i] = max(s0[i - 1], s2[i - 1]);
+            s1[i] = max(s1[i - 1], s0[i - 1] - prices[i]);
+            s2[i] = s1[i - 1] + prices[i];
+        }
+        return max(s0[n - 1], s2[n - 1]);
+    }
+};
