f39bb11ab714da082acc64da7e83aef75122e9ee
iambharatks <iambharatks@gmail.com>
2021-12-09 21:01:09 +0530

    1306. Jump Game III

--- Graph/jumpGameIII.cpp
+++ Graph/jumpGameIII.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+class Solution
+{
+public:
+    bool vis[50001] = {0};
+    bool canReach(vector<int> &arr, int start)
+    {
+       if (arr[start] == 0)
+            return true;
+        if (vis[start])
+            return false;
+        else
+            vis[start] = true;
+        return (start- arr[start] >= 0 && canReach(arr, start - arr[start])) || (start + arr[start] < arr.size() && canReach(arr, start + arr[start]));
+    }
+};
 class Solution
 {
 public:
@@ -9,22 +23,23 @@ public:
     {
         queue<int> q;
         q.push(start);
-        unordered_set<int> vis;
-        vis.insert(start);
+        bool vis[arr.size()];
+        memset(vis, 0, sizeof(vis));
+        vis[start] = 1;
         while (!q.empty())
         {
             int v = q.front();
             q.pop();
             if (arr[v] == 0)
                 return true;
-            if (v - arr[v] >= 0 && vis.find(v - arr[v]) == vis.end())
+            if (v - arr[v] >= 0 && vis[v - arr[v]] == 0)
             {
-                vis.insert(v - arr[v]);
+                vis[v - arr[v]] = 1;
                 q.push(v - arr[v]);
             }
-            if (v + arr[v] < arr.size() && vis.find(v + arr[v]) == vis.end())
+            if (v + arr[v] < arr.size() && vis[v + arr[v]] == 0)
             {
-                vis.insert(v + arr[v]);
+                vis[v + arr[v]] = 1;
                 q.push(v + arr[v]);
             }
         }
