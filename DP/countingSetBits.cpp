1392ef5238ad009647ec4df059bc4aaac516fc36
iambharatks <iambharatks@gmail.com>
2021-12-11 00:39:05 +0530

    338. Counting Bits

---
+++ DP/countingSetBits.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> countBits(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = dp[i & (i - 1)] + 1;
+        }
+        return dp;
+    }
+};
