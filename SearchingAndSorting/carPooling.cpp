82c44d11594a6370f05204d965dcd76fe0684db2
iambharatks <iambharatks@gmail.com>
2022-01-06 17:01:26 +0530

    1094. Car Pooling

--- SearchingAndSorting/carPooling.cpp
+++ SearchingAndSorting/carPooling.cpp
@@ -1,32 +1,60 @@
-#include <bits/stdc++.h> 
- 
+#include <bits/stdc++.h>
+
 using namespace std;
- 
-class Solution {
+
+class Solution
+{
 public:
-    bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<pair<int,int>> enter(trips.size()),leave(trips.size());
-        for(int i= 0 ; i < trips.size() ; i++){
-            enter[i] = {trips[i][1],i};
-            leave[i] = {trips[i][2],i};
+    bool carPooling(vector<vector<int>> &trips, int capacity)
+    {
+        vector<pair<int, int>> enter(trips.size()), leave(trips.size());
+        for (int i = 0; i < trips.size(); i++)
+        {
+            enter[i] = {trips[i][1], i};
+            leave[i] = {trips[i][2], i};
         }
-        int i = 0 , j = 0,n = trips.size();
+        int i = 0, j = 0, n = trips.size();
         int curCap = 0;
-        sort(enter.begin(),enter.end());
-        sort(leave.begin(),leave.end());
-        while(i < n && j < n){
-            if(enter[i].first < leave[j].first){
+        sort(enter.begin(), enter.end());
+        sort(leave.begin(), leave.end());
+        while (i < n && j < n)
+        {
+            if (enter[i].first < leave[j].first)
+            {
                 curCap += trips[enter[i].second][0];
                 i++;
-            }else{
+            }
+            else
+            {
                 curCap -= trips[leave[j].second][0];
                 j++;
             }
-            if(curCap > capacity)
+            if (curCap > capacity)
                 return false;
         }
-        while(j < n)
+        while (j < n)
             curCap -= trips[leave[j++].second][0];
         return curCap <= capacity;
+    }
+};
+class Solution
+{
+public:
+    bool carPooling(vector<vector<int>> &trips, int capacity)
+    {
+        vector<int> stops(1001, 0);
+        for (vector<int> &trip : trips)
+        {
+            stops[trip[1]] += trip[0];
+            stops[trip[2]] -= trip[0];
+        }
+        int curCapacity = 0;
+        for (int &i : stops)
+        {
+            curCapacity += i;
+            if (curCapacity > capacity)
+                return false;
+        }
+        return true;
     }
 };
