abdb00ad92d56e1895e575fc167784fc163c154e
iambharatks <iambharatks@gmail.com>
2022-02-17 18:12:41 +0530

    39. Combination Sum

--- Backtracking/combinationalSum.cpp
+++ Backtracking/combinationalSum.cpp
@@ -1,28 +1,53 @@
-#include <bits/stdc++.h> 
- 
+#include <bits/stdc++.h>
+
 using namespace std;
- 
-class Solution {
+class Solution
+{
+public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        vector<vector<vector<int>>> dp(target + 1);
+        dp[0] = {{}};
+        for (int &candidate : candidates)
+        {
+            for (int tot = candidate; tot <= target; tot++)
+            {
+                for (auto v : dp[tot - candidate])
+                {
+                    v.push_back(candidate);
+                    dp[tot].push_back(v);
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+class Solution
+{
 public:
     vector<vector<int>> res;
-    void rec(vector<int> &candidates,int N,int target,vector<int> &temp){
-        if(target == 0){
-            if(!temp.empty())
+    void rec(vector<int> &candidates, int N, int target, vector<int> &temp)
+    {
+        if (target == 0)
+        {
+            if (!temp.empty())
                 res.push_back(temp);
             return;
         }
-        if(N == 0)
+        if (N == 0)
             return;
-        if(candidates[N-1] <= target){
-            temp.push_back(candidates[N-1]);
-            rec(candidates,N,target-candidates[N-1],temp);
+        if (candidates[N - 1] <= target)
+        {
+            temp.push_back(candidates[N - 1]);
+            rec(candidates, N, target - candidates[N - 1], temp);
             temp.pop_back();
         }
-        rec(candidates,N-1,target,temp);
+        rec(candidates, N - 1, target, temp);
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
         vector<int> temp;
-        rec(candidates,candidates.size(),target,temp);
+        rec(candidates, candidates.size(), target, temp);
         return res;
     }
 };
