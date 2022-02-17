1dca679f80371921d8683db0371759c8fbc2a6a3
iambharatks <iambharatks@gmail.com>
2022-02-17 17:58:52 +0530

    39. Combination Sum

---
+++ Backtracking/combinationalSum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+ 
+using namespace std;
+ 
+class Solution {
+public:
+    vector<vector<int>> res;
+    void rec(vector<int> &candidates,int N,int target,vector<int> &temp){
+        if(target == 0){
+            if(!temp.empty())
+                res.push_back(temp);
+            return;
+        }
+        if(N == 0)
+            return;
+        if(candidates[N-1] <= target){
+            temp.push_back(candidates[N-1]);
+            rec(candidates,N,target-candidates[N-1],temp);
+            temp.pop_back();
+        }
+        rec(candidates,N-1,target,temp);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        rec(candidates,candidates.size(),target,temp);
+        return res;
+    }
+};
